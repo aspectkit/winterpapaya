e1404353f64a1aebd26d468c87f06e0ee030a2b3
kit <dhinkit@gmail.com>
2023-04-16 17:49:11 -0600

    added navbar

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,18 @@
 let spot = "welcome"
 $('#logo-btn').click(function() {
     $('#welcome-btn').fadeIn('slow');
+    fadOutContent();
     spot = "welcome"
 })
 
 function fadeInContent(){
     //TODO: create navbar where logo is with sections for home (show works), commissions (assuming this is price sheet), and contact page
+    $('#navdisplay').fadeIn('slow');
 }
 
-
+function fadOutContent(){
+    $('#navdisplay').fadeOut('fast');
+}
 
 
 
